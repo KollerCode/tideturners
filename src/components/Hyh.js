ebf72ebf6a4693b515d8f43f302b1baef9dcc0dd
Natasha Koller <kollernatasha@gmail.com>
2023-11-26 12:39:38 -0500

    topbar complete

--- src/components/Hyh.js
+++ src/components/Hyh.js
@@ -27,16 +27,33 @@ function Hyh() {
   }
   window.addEventListener("scroll", reveal);
   reveal()
+
+  function foreground() {
+    var actions = document.querySelectorAll(".foreground");
+    for (var i = 0; i < actions.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = actions[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        actions[i].classList.add("active");
+      } else {
+        actions[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", foreground);
+  foreground();
   
   return (
     <div className="">
-      <img src={bg} className="topBanner img-fluid"></img>
-      <img src={logo} className="hyhLogo img-fluid"></img>
-      <img src={mg} className="topBannerMid reveal"></img>
-      {/* <img src={fg} className="topBannerMid reveal"></img> */}
+      <div className="section-1">
+        <img src={bg} className="topBanner img-fluid"></img>
+        <img src={logo} className="hyhLogo img-fluid"></img>
+        <img src={mg} className="topBannerMid reveal"></img>
+        <img src={fg} className="foreground"></img>
+      </div>
       <Games></Games>
     </div>
-    
   ); 
   
 }
